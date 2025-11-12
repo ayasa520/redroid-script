9b310aa4de8979d0a30ee71e6af39e2321f6ab60
rikka <ayasa0520@gmail.com>
2025-11-12 12:15:18 +0800

    add android 14

--- redroid.py
+++ redroid.py
@@ -22,7 +22,7 @@ def main():
                         dest='android',
                         help='Specify the Android version to build',
                         default='11.0.0',
-                        choices=['13.0.0', '12.0.0', '12.0.0_64only', '11.0.0', '10.0.0', '9.0.0', '8.1.0'])
+                        choices=['14.0.0', '13.0.0', '12.0.0', '12.0.0_64only', '11.0.0', '10.0.0', '9.0.0', '8.1.0'])
     parser.add_argument('-g', '--install-gapps',
                         dest='gapps',
                         help='Install OpenGapps to ReDroid',
